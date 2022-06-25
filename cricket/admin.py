571e003aa8d1bfb970b36372291b0542c5ff28ab
python_dev <64308925+python-dev1@users.noreply.github.com>
2022-06-25 20:20:43 +0500

    Add files via upload

--- cricket/admin.py
+++ cricket/admin.py
@@ -1,13 +1,15 @@
-
 from django.contrib import admin
-from .models import player,filedatacheck,userprofile,sha1hash,sha256hash,md5hash,filedata
+from .models import player,filedatacheck,userprofile,sha1hash,sha256hash,md5hash,filedata,dataset,validfiles
 
 # Register your models here.
 
 
 
 admin.site.register(userprofile)
-admin.site.register(filedata)
 admin.site.register(sha256hash)
 admin.site.register(sha1hash)
 admin.site.register(md5hash)
+admin.site.register(dataset)
+admin.site.register(validfiles)
+
+
