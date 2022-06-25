3fe1f1d029dfe1f82ea697cca77418851cda969d
python_dev <64308925+python-dev1@users.noreply.github.com>
2022-06-25 17:35:27 +0500

    Add files via upload

---
+++ models.py
@@ -0,0 +1,39 @@
+from django.db import models
+
+# Create your models here.
+
+class player(models.Model):
+    name=models.CharField(max_length=200)
+    age=models.CharField(max_length=200)
+
+class filedata(models.Model):
+    myfile=models.FileField(upload_to='documents/%Y/%m/%d')
+
+
+class filedatacheck(models.Model):
+    myfile=models.FileField(upload_to='documents/%Y/%m/%d')
+    sha1 = models.CharField(max_length=400)
+    sha256 = models.CharField(max_length=400)
+    md5=models.CharField(max_length=400)
+
+class userprofile(models.Model):
+    name=models.CharField(max_length=200)
+    username=models.CharField(max_length=200)
+    email=models.CharField(max_length=200)
+    password=models.CharField(max_length=200)
+
+class sha256hash(models.Model):
+    value=models.CharField(max_length=400)
+
+class sha1hash(models.Model):
+    value=models.CharField(max_length=400)
+
+class md5hash(models.Model):
+    value=models.CharField(max_length=400)
+
+
+
+
+
+
+
