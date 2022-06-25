ce5f5e502c06dab8412c2f0c5357d6bb4e66caee
python_dev <64308925+python-dev1@users.noreply.github.com>
2022-06-25 17:33:04 +0500

    Update admin.py

--- cricket/admin.py
+++ cricket/admin.py
@@ -1,3 +1,13 @@
+
 from django.contrib import admin
+from .models import player,filedatacheck,userprofile,sha1hash,sha256hash,md5hash,filedata
 
 # Register your models here.
+
+
+
+admin.site.register(userprofile)
+admin.site.register(filedata)
+admin.site.register(sha256hash)
+admin.site.register(sha1hash)
+admin.site.register(md5hash)
